f4187b1aeb6e7a37b50faa904d65b309bf6a9edb
Jmackas <jamesrossmackay@gmail.com>
2019-05-01 17:19:34 +0800

    Mining lifecycle accordion styling

--- mockup_injection/script.js
+++ mockup_injection/script.js
@@ -3,9 +3,12 @@ MiningLifeCycle();
 function MiningLifeCycle() {
     document.querySelector("#hotspot-5781").remove();
 
-    let targettedNode = document.querySelector(".content");
+    let targettedNode = document.querySelector(".wrap");
 
     // Insert the button for downloading the button
-    targettedNode.insertAdjacentHTML('afterend', '');
+    targettedNode.insertAdjacentHTML('beforeend', '<iframe class="lifecycle_accordion" src="https://jmackas.github.io/Scope-Systems/accordion_prototype/"></iframe>');
+    
+    // Insert the iframe styling
+    targettedNode.insertAdjacentHTML('beforeend', 'iframe.lifecycle_accordion {width: 100%;height: 1500px;}');
 
 }
