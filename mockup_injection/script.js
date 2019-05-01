e04d6fb04abbf22c72ba20475213c6c6773e2e2f
Jmackas <jamesrossmackay@gmail.com>
2019-05-01 17:29:12 +0800

    Styles for the mining lifecycle iframe added

--- mockup_injection/script.js
+++ mockup_injection/script.js
@@ -17,6 +17,6 @@ function MiningLifeCycle() {
     targettedNode.insertAdjacentHTML('beforeend', '<iframe class="lifecycle_accordion" src="https://jmackas.github.io/Scope-Systems/accordion_prototype/"></iframe>');
     
     // Insert the iframe styling
-    targettedNode.insertAdjacentHTML('beforeend', '<style>iframe.lifecycle_accordion {width: 100%;height: 1500px;}</style>');
+    targettedNode.insertAdjacentHTML('beforeend', '<style>');
 
 }
